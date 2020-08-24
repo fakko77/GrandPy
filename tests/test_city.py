96bf7851edd57223e9d66018ff0518ee25eb632c
morgan <facorat.morgan@gmail.com>
2020-08-24 18:44:59 +0200

    test add

---
+++ tests/test_city.py
@@ -0,0 +1,24 @@
+from model.City import City
+
+def test_searchCity():
+    # ville = City("Paris")
+    assert "0" == "0"
+
+    # """
+    # >>>
+    # >>>
+    # location =
+    # """
+
+# -City:
+#  -Modifier nom ville
+#  - Recuperer location
+#  - recuper extract
+
+# -Parser
+#   -ajouter dico et txt
+#   -
+#
+#
+#
+#
